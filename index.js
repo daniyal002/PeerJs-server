5d16fde4b26a715b785950b720a2256ec9b34c0e
daniyal002 <daniyalou@gmail.com>
2025-09-17 13:41:38 +0300

    init peerJs server

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { ExpressPeerServer } from "peer";
+
+const app = express();
+
+const server = app.listen(9000, () => {
+  console.log("✅ PeerJS server running on port 9000");
+});
+
+// Настраиваем PeerJS
+const peerServer = ExpressPeerServer(server, {
+  path: "/peerjs",
+  allow_discovery: true,
+});
+
+app.use("/peerjs", peerServer);
+
+app.get("/", (req, res) => {
+  res.send("PeerJS server is running");
+});
